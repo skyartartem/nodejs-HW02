6ed936fdf505d4b0af1ebb25175ce0ee794b6e6d
Artem Riabchych <artemskyart@gmail.com>
2023-05-12 16:55:30 +0300

    add-helper-function

---
+++ helpers/HttpError.js
@@ -0,0 +1,14 @@
+const messages = {
+    400: "Bad request",
+    401: "Unautorized",
+    403: "Forbidden",
+    404: "Not Found",
+    409: "Conflict",
+}
+const HttpError = (status, message= messages[status]) => {
+    const error = new Error(message)
+    error.status = status;
+    return error
+}
+
+module.export = HttpError
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,6 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 const {
   listContacts,
   getContactById,
@@ -9,27 +9,49 @@ const {
   updateContact,
 } = require("../../models/contacts");
 
-router.get('/', async (req, res, next) => {
-  const result = await listContacts()
-  res.json(result)
-})
+const {HttpError} = require("../../helpers")
 
-router.get('/:contactId', async (req, res, next) => {
-  const id = req.params.contactId;
-  const result = await getContactById(id);
-  res.json(result);
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await listContacts();
+    res.json(result);
+  } catch (error) {
+    // res.status(500).json({ message: "Server error" });
+    next(error)
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const id = req.params.contactId;
+    const result = await getContactById(id);
+    if (!result) {
+      throw HttpError(404, `Contact with id:${id} not found`)
+      // const error = new Error(`Contact with id:${id} not found`);
+      // error.status = 404;
+      // throw error;
+      // return res
+      //   .status(404)
+      //   .json({ message: `Contact with id:${id} not found` });
+    }
+    res.json(result);
+  } catch (error) {
+    // const { status = 500, message = "Server error" } = error;
+    // res.status(status).json({ message });
+     next(error);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-module.exports = router
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+module.exports = router;
