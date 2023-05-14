652cc33ab51b6b3c8afd49c2f861bad836b35e02
Artem Riabchych <artemskyart@gmail.com>
2023-05-14 18:21:22 +0300

    fix-error-messages

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -21,9 +21,20 @@ const getContact = async (req, res) => {
     const id = req.params.contactId;
     const result = await getContactById(id);
     if (!result) {
-      throw HttpError(404, `Contact with id:${id} not found`);
+      throw HttpError(404, "Not found");
     }
     res.json(result);
+};
+
+const delCont = async (req, res) => {
+    const id = req.params.contactId;
+    const result = await removeContact(id);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json({
+      message: "contact deleted",
+    });
 };
 
 const addCont = async (req, res) => {
@@ -31,23 +42,12 @@ const addCont = async (req, res) => {
     res.status(201).json(result);
 };
 
-const delCont = async (req, res) => {
-    const id = req.params.contactId;
-    const result = await removeContact(id);
-    if (!result) {
-      throw HttpError(404, `Contact with id:${id} not found`);
-    }
-    res.json({
-      message: `Object with name:${result.name} Delete success`,
-    });
-};
-
 const updateCont = async (req, res) => {
     // const { body } = req;
     const { contactId } = req.params;
     const result = await updateContact(contactId, req.body);
     if (!result) {
-      throw HttpError(404, `Contact with id:${contactId} not found`);
+      throw HttpError(404, "Not found");
     }
     res.json(result);
 };
@@ -55,7 +55,7 @@ const updateCont = async (req, res) => {
 module.exports = {
   getAllContacts: ctrlWrapper(getAllContacts),
   getContact: ctrlWrapper(getContact),
-  addCont: ctrlWrapper(addCont),
   delCont: ctrlWrapper(delCont),
+  addCont: ctrlWrapper(addCont),
   updateCont: ctrlWrapper(updateCont),
 };
--- decorators/validateBody.js
+++ decorators/validateBody.js
@@ -5,7 +5,10 @@ const validateBody = schema => {
     const func = (req, res, next) => {
          const { body } = req;
          const { error } = schema.validate(body);
-         if (error) {
+        if (error) {
+             if (Object.keys(body).length === 0) {
+               throw HttpError(400, "missing fields");
+             }
            next(HttpError(400, error.message));
         }
         next()
