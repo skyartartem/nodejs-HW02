10b6861fd7d71379299632b1e6cad36e1f810e08
Artem Riabchych <artemskyart@gmail.com>
2023-05-18 17:59:43 +0300

    add_mongodb

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+const mogoose = require("mongoose")
 
-app.listen(3000, () => {
+const app = require('./app')
+// culTsecaDRxLC0vU
+const DB_HOST =
+  "mongodb+srv://skyart:culTsecaDRxLC0vU@cluster0.jkkw02m.mongodb.net/skyart?retryWrites=true&w=majority";
+mogoose.connect(DB_HOST).then(() => {
+  app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
 })
+}).catch(error => {
+  console.log(error.message);
+  process.exit(1)
+});
+
